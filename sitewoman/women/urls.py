d9b2372cbc3bf3cca840b3b1117ce84d0bc18710
acronicsM <91975262+acronicsM@users.noreply.github.com>
2023-09-07 14:43:40 +0300

    Перенаправление, шаблоны, redirect, переменные

--- sitewoman/women/urls.py
+++ sitewoman/women/urls.py
@@ -1,12 +1,13 @@
-from django.urls import path, re_path, register_converter
+from django.urls import path, register_converter
 from . import views, converters
 
 
 register_converter(converters.FourDigitYearConverter, 'year4')
 
 urlpatterns = [
-    path('', views.index),
-    path('cats/<int:cat_id>/', views.categories),
-    path('cats/<slug:cat_slug>/', views.categories_by_slug),
-    path('archive/<year4:year>/', views.archive),
+    path('', views.index, name='home'),
+    path('about/', views.about, name='about'),
+    path('cats/<int:cat_id>/', views.categories, name='cats_id'),
+    path('cats/<slug:cat_slug>/', views.categories_by_slug, name='cats'),
+    path('archive/<year4:year>/', views.archive, name='archive'),
 ]
