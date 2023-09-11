71c14d53719ff666540704048281c3599d86d2eb
acronicsM <91975262+acronicsM@users.noreply.github.com>
2023-09-11 14:08:15 +0300

    Фильтры шаблонов, теги шаблонов, тег url

--- sitewoman/women/views.py
+++ sitewoman/women/views.py
@@ -1,14 +1,24 @@
 from django.http import HttpResponse, HttpRequest, HttpResponseNotFound
-from django.shortcuts import render, redirect
-from django.urls import reverse
-from django.template.loader import render_to_string
+from django.shortcuts import render
 
-menu = ['О сайте', 'Добавить статью', 'Обратная связь', 'Войти']
+menu = [
+    {'title': 'О сайте', 'url_name': 'about'},
+    {'title': 'Добавить статью', 'url_name': 'add_page'},
+    {'title': 'Обратная связь', 'url_name': 'contact'},
+    {'title': 'Войти', 'url_name': 'login'},
+]
 
 
 class MyClass:
     def __init__(self, a, b):
         self.a, self.b = a, b
+
+
+data_db = [
+    {'id': 1, 'title': 'Анджелина Джоли', 'content': 'Биография', 'is_published': True},
+    {'id': 2, 'title': 'Марго Робби', 'content': 'Биография', 'is_published': False},
+    {'id': 3, 'title': 'Джулия Робертс', 'content': 'Биография', 'is_published': True},
+]
 
 
 def index(request: HttpRequest):
@@ -19,7 +29,8 @@ def index(request: HttpRequest):
         'lst': [1, 2, 'abc', True],
         'set': {1, 2, 3, 2, 5},
         'dict': {'key1': 'value1', 'key2': 'value2'},
-        'obj': MyClass(10, 20)
+        'obj': MyClass(10, 20),
+        'posts': data_db,
     }
     return render(request, 'women/index.html', context=data)
 
@@ -28,22 +39,20 @@ def about(request: HttpRequest):
     return render(request, 'women/about.html', {'title': 'О сайте'})
 
 
-def categories(request: HttpRequest, cat_id: int):
-    return HttpResponse(f'<h1>Статьи по категориям</h1><p>id: {cat_id}</p>')
+def show_post(request: HttpRequest, post_id: int):
+    return HttpResponse(f'Отображение статьи с id = {post_id}')
 
 
-def categories_by_slug(request: HttpRequest, cat_slug: str):
-    if request.GET:
-        print(request.GET)
-    return HttpResponse(f'<h1>Статьи по категориям</h1><p>slug: {cat_slug}</p>')
+def addpage(request: HttpRequest):
+    return HttpResponse(f'Добавление статьи')
 
 
-def archive(request: HttpRequest, year: int):
-    if year > 2023:
-        uri = reverse('cats', args=('music', ))
-        return redirect(uri)
+def contact(request: HttpRequest):
+    return HttpResponse(f'Контакты')
 
-    return HttpResponse(f'<h1>Архив по годам</h1><p>{year}</p>')
+
+def login(request: HttpRequest):
+    return HttpResponse(f'Авторизация')
 
 
 def page_not_found(request: HttpRequest, exception):
