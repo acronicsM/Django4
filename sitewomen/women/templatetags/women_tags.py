084321df1b0298718f9e5158c07698bcfc721ded
acronicsM <91975262+acronicsM@users.noreply.github.com>
2023-10-02 16:53:28 +0300

    slug, choices, models.Manager, ForeignKey,

--- sitewomen/women/templatetags/women_tags.py
+++ sitewomen/women/templatetags/women_tags.py
@@ -1,5 +1,6 @@
 from django import template
 import women.views as views
+from women.models import Category
 
 register = template.Library()
 
@@ -11,5 +12,5 @@ def get_categories():
 
 @register.inclusion_tag('women/list_categories.html')
 def show_categories(cat_selected=0):
-    cats = views.cats_db
+    cats = Category.objects.all()
     return {'cats': cats, 'cat_selected': cat_selected}
