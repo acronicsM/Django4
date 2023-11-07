21cc9f045a94b56b44607e8319e3321334e4784d
acronicsM <91975262+acronicsM@users.noreply.github.com>
2023-11-07 15:04:43 +0300

    Пользовательские поля и действия в админ-панели; Панель поиска и панель фильтрации; Настройка формы редактирования записей в админ-панели; Внешний вид админки; HTML-формы;

--- sitewomen/women/models.py
+++ sitewomen/women/models.py
@@ -1,5 +1,16 @@
 from django.db import models
 from django.urls import reverse
+from django.template.defaultfilters import slugify
+
+
+def translit_to_eng(s: str) -> str:
+    d = {'а': 'a', 'б': 'b', 'в': 'v', 'г': 'g', 'д': 'd',
+         'е': 'e', 'ё': 'yo', 'ж': 'zh', 'з': 'z', 'и': 'i', 'к': 'k',
+         'л': 'l', 'м': 'm', 'н': 'n', 'о': 'o', 'п': 'p', 'р': 'r',
+         'с': 's', 'т': 't', 'у': 'u', 'ф': 'f', 'х': 'h', 'ц': 'c', 'ч': 'ch',
+         'ш': 'sh', 'щ': 'shch', 'ь': '', 'ы': 'y', 'ъ': '', 'э': 'r', 'ю': 'yu', 'я': 'ya'}
+
+    return "".join(map(lambda x: d[x] if d.get(x, False) else x, s.lower()))
 
 
 class PublishedManager(models.Manager):
@@ -19,7 +30,7 @@ class Women(models.Model):
     time_update = models.DateTimeField(auto_now=True, verbose_name='Время изменения')
 
     is_published = models.BooleanField(choices=tuple(map(lambda x: (bool(x[0]), x[1]), Status.choices)),
-                                       default=Status.PUBLISHED,
+                                       default=Status.DRAFT,
                                        verbose_name='Статус')
 
     cat = models.ForeignKey('Category', on_delete=models.PROTECT, related_name='posts', verbose_name='Категория')
@@ -47,6 +58,10 @@ class Women(models.Model):
 
     def get_absolute_url(self):
         return reverse('post', kwargs={'post_slug': self.slug})
+
+    # def save(self, *args, **kwargs):
+    #     self.slug = slugify(translit_to_eng(self.title))
+    #     super().save(*args, **kwargs)
 
 
 class Category(models.Model):
--- sitewomen/women/views.py
+++ sitewomen/women/views.py
@@ -4,6 +4,7 @@ from django.urls import reverse
 from django.template.loader import render_to_string
 from django.template.defaultfilters import slugify
 
+from .forms import AddPostForm
 from .models import Women, Category, TagPost
 
 menu = [{'title': "О сайте", 'url_name': 'about'},
@@ -42,7 +43,24 @@ def show_post(request, post_slug):
 
 
 def addpage(request):
-    return HttpResponse("Добавление статьи")
+
+    if request.method == 'POST':
+        form = AddPostForm(request.POST)
+        if form.is_valid():
+            try:
+                Women.objects.create(**form.cleaned_data)
+                return redirect('home')
+            except:
+                form.add_error(None, 'Ошибка добавления поста')
+    else:
+        form = AddPostForm()
+
+    data = {
+        'title': 'Добавление статьи',
+        'menu': menu,
+        'form': form,
+    }
+    return render(request, 'women/addpage.html', data)
 
 
 def contact(request):
