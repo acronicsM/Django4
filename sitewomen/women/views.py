eb4887ff2983c955de64cb3871f030726639bdf3
acronicsM <91975262+acronicsM@users.noreply.github.com>
2023-11-13 14:12:55 +0300

    FormView; CreateView;

--- sitewomen/women/views.py
+++ sitewomen/women/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse, HttpResponseNotFound
-from django.shortcuts import render, redirect, get_object_or_404
-from django.views import View
-from django.views.generic import ListView, DetailView
+from django.shortcuts import render, get_object_or_404
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
 
 from .forms import AddPostForm, UploadFileForm
 from .models import Women, TagPost, UploadFiles
@@ -69,28 +69,27 @@ class ShowPost(DetailView):
         return get_object_or_404(Women.published, slug=self.kwargs[self.slug_url_kwarg])
 
 
-class AddPage(View):
-    def get(self, request):
-        form = AddPostForm()
-        data = {
+class AddPage(CreateView):
+    form_class = AddPostForm
+    template_name = 'women/addpage.html'
+    success_url = reverse_lazy('home')
+
+    extra_context = {
             'title': 'Добавление статьи',
             'menu': menu,
-            'form': form,
         }
-        return render(request, 'women/addpage.html', data)
 
-    def post(self, request):
-        form = AddPostForm(request.POST, request.FILES)
-        if form.is_valid():
-            form.save()
-            return redirect('home')
 
-        data = {
-            'title': 'Добавление статьи',
-            'menu': menu,
-            'form': form,
-        }
-        return render(request, 'women/addpage.html', data)
+class UpdatePage(UpdateView):
+    model = Women
+    fields = ['title', 'content', 'photo', 'is_published', 'cat']
+    template_name = 'women/addpage.html'
+    success_url = reverse_lazy('home')
+
+    extra_context = {
+        'title': 'Редактирование статьи',
+        'menu': menu,
+    }
 
 
 def contact(request):
